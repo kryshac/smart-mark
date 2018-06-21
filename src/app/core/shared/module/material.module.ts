d0f51261609991ae651f42a36cfbb65947084d99
Cazacu Cristian <vladislav.tornea@gmail.com>
2018-06-22 00:16:22 +0300

    fix import module

--- src/app/core/shared/module/material.module.ts
+++ src/app/core/shared/module/material.module.ts
@@ -2,31 +2,31 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { NgModule } from '@angular/core';
 import {
   MatButtonModule,
+  MatGridListModule,
   MatIconModule,
   MatListModule,
   MatSidenavModule,
   MatToolbarModule,
 } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
-    BrowserAnimationsModule,
     LayoutModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatGridListModule,
   ],
   exports: [
-    BrowserAnimationsModule,
     LayoutModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatGridListModule,
   ],
   declarations: [],
 })
