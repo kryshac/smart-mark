792f71d6cfd417d0c33bcd3a8a01b8a97e2a3d7d
Cristian Cazacu <vladislav.tornea@gmail.com>
2018-06-21 19:59:25 +0300

    import store module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
+
+import { GraphQLModule } from '@app/core/module';
 import { MaterialModule } from '@app/core/shared';
+import { StoreCoreModule } from '@app/core/store/store.module';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, GraphQLModule, StoreCoreModule.forRoot()],
   declarations: [],
   exports: [CommonModule, MaterialModule],
   providers: [],
