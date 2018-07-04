75a5c38fa12ffc77d638ac221188fe0d241fecf8
Cristian Cazacu <vladislav.tornea@gmail.com>
2018-07-04 21:50:16 +0300

    add component to remove bookmark

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { AddBookmarkButtonComponent, NavbarColumnComponent } from './components';
 import { AddBookmarkComponent } from './components/add-bookmark/add-bookmark.component';
 import { DialogAlertComponent } from './components/dialog-alert/dialog-alert.component';
+import { RemoveBookmarkComponent } from './components/remove-bookmark/remove-bookmark.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { DialogAlertComponent } from './components/dialog-alert/dialog-alert.com
     AddBookmarkButtonComponent,
     AddBookmarkComponent,
     DialogAlertComponent,
+    RemoveBookmarkComponent,
   ],
   imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, CoreModule],
   providers: [],
